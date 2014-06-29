b49c9f9472bc072b9b7b23e2cf9cba14884e7980
Matt Barry <mrbcuda@mac.com>
2014-06-29 15:00:50 -0500

    Added summary function

--- R/stressr.R
+++ R/stressr.R
@@ -17,10 +17,9 @@ getStressIndex <- function(verbose=FALSE) {
   
   ciurl <- "http://www.clevelandfed.org/research/data/financial_stress_index/cfsi-dataset.cfm"
   
-  # TODO read components from web
   if ( verbose )
     message("Reading CFSI data...")
-  x <- readHTMLTable('data/cfsi-data.html',
+  x <- readHTMLTable(ciurl,
                      header=TRUE,
                      skip.rows=1,
                      colClasses=c("character",rep("numeric",22)),
@@ -65,42 +64,67 @@ getStressIndex <- function(verbose=FALSE) {
 getStressComponents <- function(verbose=FALSE) {
   
   cfurl <- "http://www.clevelandfed.org/research/data/financial_stress_index/component-dataset.cfm"
-
-  # TODO read components from web
+  
   if (verbose)
     message("Reading component data...")
-  x <- readHTMLTable('data/component-data.html',
-                    header=TRUE,
-                    skip.rows=1,
-                    colClasses=c("character",rep("numeric",22)),
-                    stringsAsFactors=FALSE)
+  x <- readHTMLTable(cfurl,
+                     header=TRUE,
+                     skip.rows=1,
+                     colClasses=c("character",rep("numeric",22)),
+                     stringsAsFactors=FALSE)
   components <- x[[1]]
   colnames(components) <- c(
-      "DATE",
-      "ASSET-BACKED SECURITY SPREAD",
-      "BANK BOND SPREAD",
-      "COMMERCIAL MBS SPREAD",
-      "COMMERCIAL PAPER - T-BILL SPREAD",
-      "COMMERCIAL REAL ESTATE SPREAD",
-      "CORPORATE BOND SPREAD",
-      "COVERED INTEREST SPREAD",
-      "CREDIT MARKETS",
-      "EQUITY MARKETS",
-      "FINANCIAL BETA",
-      "FOREIGN EXCHANGE MARKETS",
-      "INTERBANK COST OF BORROWING",
-      "INTERBANK LIQUIDITY SPREAD",
-      "INTERBANK MARKETS",
-      "LIQUIDITY SPREAD",
-      "REAL ESTATE MARKET",
-      "RESIDENTIAL MBS SPREAD",
-      "RESIDENTIAL REAL ESTATE SPREAD",
-      "SECURITIZATION MARKET",
-      "STOCK MARKET CRASHES",
-      "TREASURY YIELD CURVE SPREAD",
-      "WEIGHTED DOLLAR CRASHES"
+    "DATE",
+    "ASSET-BACKED SECURITY SPREAD",
+    "BANK BOND SPREAD",
+    "COMMERCIAL MBS SPREAD",
+    "COMMERCIAL PAPER - T-BILL SPREAD",
+    "COMMERCIAL REAL ESTATE SPREAD",
+    "CORPORATE BOND SPREAD",
+    "COVERED INTEREST SPREAD",
+    "CREDIT MARKETS",
+    "EQUITY MARKETS",
+    "FINANCIAL BETA",
+    "FOREIGN EXCHANGE MARKETS",
+    "INTERBANK COST OF BORROWING",
+    "INTERBANK LIQUIDITY SPREAD",
+    "INTERBANK MARKETS",
+    "LIQUIDITY SPREAD",
+    "REAL ESTATE MARKET",
+    "RESIDENTIAL MBS SPREAD",
+    "RESIDENTIAL REAL ESTATE SPREAD",
+    "SECURITIZATION MARKET",
+    "STOCK MARKET CRASHES",
+    "TREASURY YIELD CURVE SPREAD",
+    "WEIGHTED DOLLAR CRASHES"
+  )
+  
+  fedColors <- c(
+    "#c1ad97", # "ASSET-BACKED SECURITY SPREAD",
+    "#e4aa71", # "BANK BOND SPREAD",
+    "#a8927a", # "COMMERCIAL MBS SPREAD",
+    "#537f9a", # "COMMERCIAL PAPER - T-BILL SPREAD",
+    "#597b6a", # "COMMERCIAL REAL ESTATE SPREAD",
+    "#d0e7f7",# "CORPORATE BOND SPREAD",
+    "#40657c", # "COVERED INTEREST SPREAD",
+    "#5e8aa5", # "CREDIT MARKETS",
+    "#dad38d", # "EQUITY MARKETS",
+    "#bc8146", # "FINANCIAL BETA",
+    "#999999", # "FOREIGN EXCHANGE MARKETS",
+    "#d0945a", # "INTERBANK COST OF BORROWING",
+    "#fac38e", #"INTERBANK LIQUIDITY SPREAD",
+    "#a0c81d", # "INTERBANK MARKETS",
+    "#324754", # "LIQUIDITY SPREAD",
+    "#2ba4a5", # "REAL ESTATE MARKET",
+    "#97846e", # "RESIDENTIAL MBS SPREAD",
+    "#6e9180", # "RESIDENTIAL REAL ESTATE SPREAD",
+    "#df6968", #" SECURITIZATION MARKET",
+    "#dbd48e", # "STOCK MARKET CRASHES",
+    "#90b7cf", # "TREASURY YIELD CURVE SPREAD",
+    "#999999" # WEIGHTED DOLLAR CRASHES"
   )
 
+  
   # convert date posted to date class
   components$DATE <- as.Date(components$DATE,format="%m/%d/%Y")
   components <- xts(components[,-1],order.by=components[,1])
@@ -108,12 +132,53 @@ getStressComponents <- function(verbose=FALSE) {
     message("Completed component data.")
   
   rv <- list(df=components,
-             colors=1:ncol(components),
+             colors=fedColors,
              columns=1:ncol(components),
              main="Financial Stress Index",
              ylab="")
   class(rv) <- "stress"
   rv
+}
+
+#' @title Get stress components summary
+#' @description Downloads FRB financial stress index component data.
+#' @details Downloads the Cleveland FRB data products for financial stress index components daily time series.  Component values include
+#' \itemize{
+#' \item foreign exchange markets
+#' \item credit markets
+#' \item interbank markets
+#' \item equity markets
+#' \item real estate market
+#' \item securitization market
+#' }
+#' @param s the list of class \code{stress} from previous queries, or NULL to perform new query
+#' @return A list of class \code{stress}
+#' @export
+#' @seealso getStressData getEquityMarkets getFundingMarkets getCreditMarkets getForeignExchangeMarkets getRealEstateMarkets getSecuritizationMarkets
+#' @examples
+#' \dontrun{
+#' getEquityMarkets()
+#' }
+getComponentSummary <- function(s=NULL) {
+  stopifnot(class(s)=="stress" || is.null(s))
+  
+    fedColors <- c(
+      "#999999", # "FOREIGN EXCHANGE MARKETS"
+      "#5e8aa5", # "CREDIT MARKETS"
+      "#a0c81d", # "INTERBANK MARKETS"
+      "#dad38d", # "EQUITY MARKETS"
+      "#2ba4a5", # "REAL ESTATE MARKET"
+      "#df6968"  #" SECURITIZATION MARKET"
+    )
+  
+  if ( is.null(s) )
+    s <- getStressComponents()
+  
+  s$columns = c(11,8,14,9,16,19)
+  s$colors = fedColors
+  s$main="Components of the CFSI - Summary"
+  s$ylab=""
+  s
 }
 
 #' @title Get equity markets stress components
